1e5293008c46dbdbf2e2debfec0f74773d3bc0e6
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-07-10 00:31:34 +0530

    Solution for UC-2

--- employee-payroll/console.sql
+++ employee-payroll/console.sql
@@ -3,3 +3,12 @@
 #Use-Case-1
 CREATE DATABASE payroll_service;
 USE payroll_service;
+
+#Use-Case-2
+CREATE TABLE employee_payroll
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(50),
+    salary     INT,
+    start_date DATE
+);
