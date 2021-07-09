d671005d5558ba395e0c20d25a1bece03c238018
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-07-10 00:32:37 +0530

    Solution for UC-3

--- employee-payroll/console.sql
+++ employee-payroll/console.sql
@@ -12,3 +12,12 @@ CREATE TABLE employee_payroll
     salary     INT,
     start_date DATE
 );
+
+#Use-Case-3
+INSERT INTO employee_payroll(name, salary, start_date)
+VALUES ('Paresh', 80000, '2021-06-25'),
+       ('Atharva', 87000, '2021-06-28'),
+       ('Rahul', 89900, '2021-06-30'),
+       ('Poonam', 99000, '2021-07-05'),
+       ('Pratham', 70000, '2021-07-01'),
+       ('Rutuja', 65000, '2021-07-06');
